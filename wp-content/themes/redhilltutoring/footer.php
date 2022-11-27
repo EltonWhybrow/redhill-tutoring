0dab2bbed1edd1fc385391f053348aa9ad6c92d0
Elton <eltonwhybrow@icloud.com>
2022-11-27 09:41:42 +0000

    fix: windows logo svg font

--- wp-content/themes/redhilltutoring/footer.php
+++ wp-content/themes/redhilltutoring/footer.php
@@ -4,8 +4,8 @@
 
         <div class="link-items flex flex-col sm:flex-row flex-wrap items-center sm:items-start justify-between p-2 text-white space-y-14 md:space-y-0">
             <div class="sm:w-full p-0 sm:p-24">
-                <img class="m-auto hidden md:block" src="/resources/img/pages/logos/redhill-tuts-logo-inverted-sm.svg" alt="Redhill Tutoring" width="340px" height="auto">
-                <img class="m-auto md:hidden" src="/resources/img/pages/logos/redhill-tuts-logo-inverted-sm.png" alt="Redhill Tutoring" width="340px" height="auto">
+                <!-- <img class="m-auto hidden md:block" src="/resources/img/pages/logos/redhill-tuts-logo-inverted-sm.svg" alt="Redhill Tutoring" width="340px" height="auto"> -->
+                <img class="m-auto" src="/resources/img/pages/logos/redhill-tuts-logo-inverted-sm.png" alt="Redhill Tutoring" width="340px" height="auto">
 
             </div>
 
