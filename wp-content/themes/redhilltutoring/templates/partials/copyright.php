2aa1d5f79f5032e01bc9411cc59829992c93ea1a
Elton <eltonwhybrow@icloud.com>
2022-12-22 11:56:03 +0000

    chore: remove limited

--- wp-content/themes/redhilltutoring/templates/partials/copyright.php
+++ wp-content/themes/redhilltutoring/templates/partials/copyright.php
@@ -1,6 +1,6 @@
 <div class="flex flex-wrap lg:container lg:mx-auto justify-center p-4">
     <p class="text-white pt-20 text-xs text-center">
-        All rights reserved &copy;<?php echo date("Y"); ?> Redhill Tutoring Ltd <span class="hidden md:block">|</span>
+        All rights reserved &copy;<?php echo date("Y"); ?> Redhill Tutoring <span class="hidden md:block">|</span>
         <br class="md:hidden" /> Theme by <a class="primary-link text-xs" href="//widesign.co.uk" target="_blank">Wide Sign</a>
     </p>
 </div>
