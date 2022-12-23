8166dc6da4a38f6434b8b8e92e2428c8d8ebadf5
Elton <eltonwhybrow@icloud.com>
2022-12-23 17:25:21 +0000

    chore: typo img bug

--- wp-content/themes/redhilltutoring/front-page.php
+++ wp-content/themes/redhilltutoring/front-page.php
@@ -68,9 +68,9 @@
 
 			<div class="brand-swoosh object-cover w-full md:w-96 md:hidden">
 				<picture>
-					<!-- <source srcset="/resources/img/pages/services/cabin-offices.webp" type="image/webp"> -->
-					<source srcset="https://picsum.photos/640/480" type="image/jpeg">
-					<img class="w-full max-h-80 object-cover object-center rounded-lg shadow-md" src="https://picsum.photos/640/480" alt="Qualified Teacher">
+					<source srcset="/resources/img/pages/home/redhilltutoring-home-university.webp" type="image/webp">
+					<source srcset="/resources/img/pages/home/redhilltutoring-home-university.jpg" type="image/jpeg">
+					<img class="w-full max-h-80 object-cover object-center rounded-lg shadow-md" src="/resources/img/pages/home/redhilltutoring-home-university.jpg" alt="Teaching degree at University of Roehampton">
 				</picture>
 			</div>
 
