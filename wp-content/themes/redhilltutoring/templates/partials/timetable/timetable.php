142f86ed9df172259ee45357e5287604be39daad
Elton <eltonwhybrow@icloud.com>
2023-04-15 13:16:58 +0100

    fix: beth updates

--- wp-content/themes/redhilltutoring/templates/partials/timetable/timetable.php
+++ wp-content/themes/redhilltutoring/templates/partials/timetable/timetable.php
@@ -3,18 +3,18 @@
 
     <div class="my-2 px-2 w-6/12 overflow-hidden">
         <h3 class="text-teal-500 font-semibold">Monday</h3>
-        <p class="italic">Coming soon</p>
+        <p class="italic">Spaces available as of September 2023</p>
     </div>
 
     <div class="my-2 px-2 w-6/12 overflow-hidden">
         <h3 class="text-teal-500 font-semibold">Tuesday</h3>
-        <p class="italic">Coming soon</p>
+        <p class="italic">Spaces available as of September 2023</p>
 
     </div>
 
     <div class="my-2 px-2 w-6/12 overflow-hidden">
         <h3 class="text-teal-500 font-semibold">Wednesday</h3>
-        <p class="italic">Coming soon</p>
+        <p class="italic">Spaces available as of September 2023</p>
     </div>
 
     <div class="my-2 px-2 w-6/12 overflow-hidden">
@@ -31,7 +31,7 @@
 
     </div>
     <div class="my-2 px-2 w-6/12 overflow-hidden">
-        <h3 class="text-teal-500 font-semibold">Saturday <span class="text-sm">(Additional 1:1 sessions)</span></h3>
+        <h3 class="text-teal-500 font-semibold">Saturday <span class="text-sm"></span></h3>
         <p>10.00am - 11.00am</p>
         <p>11.30am - 12.30pm</p>
         <p>3.00am - 4.00pm</p>
