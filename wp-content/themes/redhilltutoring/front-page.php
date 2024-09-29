69aecb73d1563f434890752beefaaca836b8238d
Elton Whybrow <eltonwhybrow@Eltons-MacBook-Pro.local>
2024-09-29 20:51:38 +0100

    replace image and copy

--- wp-content/themes/redhilltutoring/front-page.php
+++ wp-content/themes/redhilltutoring/front-page.php
@@ -13,7 +13,7 @@
 
 			<div class="flex items-center justify-center mb-3">
 				<div class="inline-flex">
-					<a class='primary-btn pr-12 relative mr-1 transform transition duration-400 ease-in-out' href='/session-booking'>
+					<a class='primary-btn pr-12 relative mr-1 transform transition duration-400 ease-in-out' href='/contact'>
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="absolute top-3 right-3 inline-block w-6 h-6 text-gray-100">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 						</svg>
